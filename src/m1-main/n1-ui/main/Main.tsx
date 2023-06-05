7453e62e70a970601459806b91511adc6f16621c
Nikolai-Doroshenko <doroshenko.nikolai.a@gmail.com>
2023-06-05 09:44:17 +0300

    fix routes

--- src/m1-main/n1-ui/main/Main.tsx
+++ src/m1-main/n1-ui/main/Main.tsx
@@ -6,16 +6,18 @@ import RegistrationPage from "../../../m2-features/f1-auth/n2-registration/Regis
 import RecoveryPasswordPage from "../../../m2-features/f1-auth/n3-recovery-password/RecoveryPasswordPage";
 import EnteringNewPasswordPage from "../../../m2-features/f1-auth/n4-entering-new-password/EnteringNewPasswordPage";
 import NotFound from "../../../m2-features/f3-not-found/NotFound";
+import TestPage from "../../../m2-features/f0-test/TestPage";
 
 function Main() {
     return (
         <div>
             <Routes>
-                <Route path={'/'} element={<ProfilePage/>}></Route>
-                <Route path={'/login'} element={<LoginPage/>}></Route>
-                <Route path={'/registration'} element={<RegistrationPage/>}></Route>
-                <Route path={'/recovery-password'} element={<RecoveryPasswordPage/>}></Route>
-                <Route path={'/entering-new-password'} element={<EnteringNewPasswordPage/>}></Route>
+                <Route path={'/cards/test'} element={<TestPage/>}></Route>
+                <Route path={'/cards'} element={<ProfilePage/>}></Route>
+                <Route path={'/cards/login'} element={<LoginPage/>}></Route>
+                <Route path={'/cards/registration'} element={<RegistrationPage/>}></Route>
+                <Route path={'/cards/recovery-password'} element={<RecoveryPasswordPage/>}></Route>
+                <Route path={'/cards/entering-new-password'} element={<EnteringNewPasswordPage/>}></Route>
                 <Route path={'/*'} element={<NotFound/>}></Route>
             </Routes>
         </div>
---
+++ src/m2-features/f0-test/TestPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import SuperInputText from "../../m1-main/n1-ui/common/c1-SuperInputText/SuperInputText";
+import SuperButton from "../../m1-main/n1-ui/common/c2-SuperButton/SuperButton";
+import SuperCheckbox from "../../m1-main/n1-ui/common/c3-SuperCheckbox/SuperCheckbox";
+
+function TestPage() {
+    return (
+        <div>
+            <SuperInputText/>
+            <SuperButton/>
+            <SuperCheckbox/>
+        </div>
+    );
+}
+
+export default TestPage;
